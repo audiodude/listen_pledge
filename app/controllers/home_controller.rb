fbc8dc6ba587aea5bf8ecfd7e5e11828c5df1dcd
Travis Briggs <audiodude@gmail.com>
2012-07-27 21:01:50 +0000

    Link to pairing if the user has eligible songs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @pairing = Pairing.for_user(current_user)
+      @eligible_songs = Song.where(:eligible => true, :user_id => current_user.id)
     end
   end
   
